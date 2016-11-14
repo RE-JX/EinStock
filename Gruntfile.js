f2b21dd736192b78700eeefc5915020e6d043921
lhawes <doubletigerstripe@gmail.com>
2016-11-14 10:34:14 -0800

    Started Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    //jshint: {
+      //files: [ 'src/*'
+     // ],
+    //},
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/*.js']
+      }
+    },
+
+
+});
+
+
+}
