c90417f62a9dea4478165c92a26f7df817514aaa
Kevin Kim <kevink007@gmail.com>
2016-11-15 17:38:27 -0500

    Added dashboards and made welcome page more legit

--- client/welcome/welcome.js
+++ client/welcome/welcome.js
@@ -1,6 +1,6 @@
 var welcomeCtrl = function($scope) {
-    $scope.data = {};
-};
+  $scope.how = "Have you ever wanted to learn about high frequency trading? What about combining the powers of technology available in Machine Learning to see if you have the winning formula to beat the stock market?";
+}
 
 angular.module('einstock.welcome', [
   'ngMaterial'
