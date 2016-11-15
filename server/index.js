659d7c94f3f48800bdbfc902036b980a00572a4c
Aaron Stevens <astev1989@gmail.com>
2016-11-15 18:08:49 -0500

    Added pic for Aaron / route for algo page

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const port = 8080;
-const database = require('../database');
-
+const database = require('../database/index.js');
+ 
 //-----------------middleware---------------
 //------------------------------------------
 app.use(bodyParser.json());
@@ -23,6 +23,10 @@ app.use('/public', express.static(path.join(__dirname + '/../node_modules')));
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname + '/../client/index.html'));
 });
+
+app.get('/dashboard', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname + '/../client/algorithm/algorithm.html'))
+})
 
 
 database.db.sync().then(() => {
