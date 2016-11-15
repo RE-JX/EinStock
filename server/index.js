159c5e0d0c074536247213a94dc1787d4a8a6a25
Aaron Stevens <astev1989@gmail.com>
2016-11-14 20:26:33 -0500

    properly serves all files presently

--- server/index.js
+++ server/index.js
@@ -5,17 +5,24 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const port = 8080;
 
+//-----------------middleware---------------
+//------------------------------------------
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname + '/../client')));
 
+app.use('/public', express.static(path.join(__dirname + '/../node_modules')));  
 
+//-----------------routes-------------------
+//------------------------------------------
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
-app.listen(port)
+app.listen(port, () => {
+  console.log('listening on port: ', port);
+});
