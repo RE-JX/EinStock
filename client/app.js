52c17fe76fdc0bc722c3901022e686a4c5f298d1
Kevin Kim <kevink007@gmail.com>
2016-11-15 23:58:02 -0500

    Solved merge conflict

--- client/app.js
+++ client/app.js
@@ -1,4 +1,10 @@
 // All the Angular modules we'll be using, including premade as well as our own modules we've created for views on our app
+var scrollerCtrl = function($scope, $location, $anchorScroll) {
+  $scope.scrollDown = function() {
+    $location.hash('navbar');
+    $anchorScroll();
+  }
+};
 
 angular.module('einstock', [
     'einstock.services',
@@ -11,27 +17,27 @@ angular.module('einstock', [
     'highcharts-ng',
     'chart.js'
   ])
+  .controller('scrollerController', scrollerCtrl)
   // This is an Angular Material Theme setting for color schemes
   .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
       // .primaryPalette('indigo')
       // .accentPalette('orange')
   })
-
   // This is where we are routing our views
   .config(function($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/dashboard', {
-      templateUrl: '/dashboard/dashboard.html',
-    })
-    .when('/algorithm', {
-      templateUrl: '/algorithm/algorithm.html'
-    })
-    .when('/welcome', {
-      templateUrl: '/welcome/welcome.html'
-    })
-    // Right now the default page is set to the algorithm selector view
-    .otherwise({
-      redirectTo: '/welcome'
-    })
-});
+    $routeProvider
+      .when('/dashboard', {
+        templateUrl: '/dashboard/dashboard.html',
+      })
+      .when('/algorithm', {
+        templateUrl: '/algorithm/algorithm.html'
+      })
+      .when('/welcome', {
+        templateUrl: '/welcome/welcome.html'
+      })
+      // Right now the default page is set to the algorithm selector view
+      .otherwise({
+        redirectTo: '/welcome'
+      })
+  });
