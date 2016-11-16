d6b35c71ad4014f6b660306081f064adb84fc20b
Kevin Kim <kevink007@gmail.com>
2016-11-15 19:17:17 -0500

    Changed a little bit of variables on welcome page

--- client/welcome/welcome.js
+++ client/welcome/welcome.js
@@ -1,5 +1,6 @@
 var welcomeCtrl = function($scope) {
   $scope.how = "Have you ever wanted to learn about high frequency trading? What about combining the powers of technology available in Machine Learning to see if you have the winning formula to beat the stock market?";
+  $scope.disclaimer = "*Don't Trade with our algorithms. This is for educational purposes only.*";
 }
 
 angular.module('einstock.welcome', [
