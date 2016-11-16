2a90cdc28a0359a364ecda1f2db2713e5c0ae948
Kevin Kim <kevink007@gmail.com>
2016-11-16 18:36:33 -0500

    Solve merge conflict in app.js file

--- client/app.js
+++ client/app.js
@@ -1,12 +1,8 @@
 // All the Angular modules we'll be using, including premade as well as our own modules we've created for views on our app
-var scrollerCtrl = function($scope, $location, $anchorScroll) {
-  $scope.scrollDown = function() {
-    $location.hash('navbar');
-    $anchorScroll();
-  }
-};
+(function() {
+  'use strict'
 
-angular.module('einstock', [
+  angular.module('einstock', [
     'einstock.services',
     'einstock.dashboard',
     'einstock.algorithm',
@@ -18,27 +14,39 @@ angular.module('einstock', [
     'highcharts-ng',
     'chart.js'
   ])
-  .controller('scrollerController', scrollerCtrl)
-  // This is an Angular Material Theme setting for color schemes
-  .config(function($mdThemingProvider) {
-    $mdThemingProvider.theme('default')
-      // .primaryPalette('indigo')
-      // .accentPalette('orange')
-  })
-  // This is where we are routing our views
-  .config(function($routeProvider, $httpProvider) {
-    $routeProvider
-      .when('/dashboard', {
-        templateUrl: '/dashboard/dashboard.html',
-      })
-      .when('/algorithm', {
-        templateUrl: '/algorithm/algorithm.html'
-      })
-      .when('/welcome', {
-        templateUrl: '/welcome/welcome.html'
-      })
-      // Right now the default page is set to the algorithm selector view
-      .otherwise({
-        redirectTo: '/welcome'
-      })
-  });
+
+  // Pass off scrolling function to FAB button on main banner
+  .controller('ScrollerController', ScrollerController)
+    // This is an Angular Material Theme setting for color schemes
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        // .primaryPalette('indigo')
+        // .accentPalette('orange')
+    })
+    // This is where we are routing our views
+    .config(function($routeProvider, $httpProvider) {
+      $routeProvider
+        .when('/dashboard', {
+          templateUrl: '/dashboard/dashboard.html',
+        })
+        .when('/algorithm', {
+          templateUrl: '/algorithm/algorithm.html'
+        })
+        .when('/welcome', {
+          templateUrl: '/welcome/welcome.html'
+        })
+        // Right now the default page is set to the algorithm selector view
+        .otherwise({
+          redirectTo: '/welcome'
+        })
+    });
+
+  //Scroller Controller defined here
+  function ScrollerController($scope, $location, $anchorScroll) {
+    $scope.scrollDown = function() {
+      $location.hash('navbar');
+      $anchorScroll();
+    }
+  };
+})();
+
--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -1,96 +1,104 @@
-var dashboardCtrl = function() {
-  console.log('GET REQUEST FROM DASHBOARD', localStorage.getItem('data'));
-};
+(function() {
+  'use strict'
 
-var stockCtrl = function($scope, $timeout) {
-  $scope.chartConfig = {
-    options: {
-      chart: {
-        zoomType: 'x'
+  angular.module('einstock.dashboard', [
+      'ngMaterial',
+      'highcharts-ng',
+      'chart.js'
+    ])
+    // Dashboard for users to see charts
+    .controller('DashboardController', DashboardController)
+    .controller('StockController', StockController)
+    .controller('lineController', LineController);
+
+  DashboardController.$inject = ['$scope'];
+  StockController.$inject = ['$scope', '$timeout'];
+  LineController.$inject = ['$scope'];
+
+  function DashboardController() {
+    console.log('GET REQUEST FROM DASHBOARD', localStorage.getItem('data'));
+  };
+
+  function StockController($scope, $timeout) {
+    $scope.chartConfig = {
+      options: {
+        chart: {
+          zoomType: 'x'
+        },
+        rangeSelector: {
+          enabled: true
+        },
+        navigator: {
+          enabled: true
+        }
       },
-      rangeSelector: {
-        enabled: true
+      series: [],
+      title: {
+        text: 'Machine Learning'
       },
-      navigator: {
-        enabled: true
+      useHighStocks: true
+    };
+
+    $scope.chartConfig.series.push({
+      id: 1,
+      data: [
+        [1147651200000, 23.15],
+        [1147737600000, 23.01],
+        [1147824000000, 22.73],
+        [1147910400000, 22.83],
+        [1147996800000, 22.56],
+        [1148256000000, 22.88],
+        [1148342400000, 22.79],
+        [1148428800000, 23.50],
+        [1148515200000, 23.74],
+        [1148601600000, 23.72],
+        [1148947200000, 23.15],
+        [1149033600000, 22.65]
+      ]
+    }, {
+      id: 2,
+      data: [
+        [1147651200000, 25.15],
+        [1147737600000, 25.01],
+        [1147824000000, 25.73],
+        [1147910400000, 25.83],
+        [1147996800000, 25.56],
+        [1148256000000, 25.88],
+        [1148342400000, 25.79],
+        [1148428800000, 25.50],
+        [1148515200000, 26.74],
+        [1148601600000, 26.72],
+        [1148947200000, 26.15],
+        [1149033600000, 26.65]
+      ]
+    });
+  };
+
+  function LineController($scope) {
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function(points, evt) {
+      console.log(points, evt);
+    };
+    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+    $scope.options = {
+      scales: {
+        yAxes: [{
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        }, {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }]
       }
-    },
-    series: [],
-    title: {
-      text: 'Machine Learning'
-    },
-    useHighStocks: true
+    };
   };
-
-  $scope.chartConfig.series.push({
-    id: 1,
-    data: [
-      [1147651200000, 23.15],
-      [1147737600000, 23.01],
-      [1147824000000, 22.73],
-      [1147910400000, 22.83],
-      [1147996800000, 22.56],
-      [1148256000000, 22.88],
-      [1148342400000, 22.79],
-      [1148428800000, 23.50],
-      [1148515200000, 23.74],
-      [1148601600000, 23.72],
-      [1148947200000, 23.15],
-      [1149033600000, 22.65]
-    ]
-  }, {
-    id: 2,
-    data: [
-      [1147651200000, 25.15],
-      [1147737600000, 25.01],
-      [1147824000000, 25.73],
-      [1147910400000, 25.83],
-      [1147996800000, 25.56],
-      [1148256000000, 25.88],
-      [1148342400000, 25.79],
-      [1148428800000, 25.50],
-      [1148515200000, 26.74],
-      [1148601600000, 26.72],
-      [1148947200000, 26.15],
-      [1149033600000, 26.65]
-    ]
-  });
-};
-
-var lineCtrl = function($scope) {
-  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-  $scope.onClick = function(points, evt) {
-    console.log(points, evt);
-  };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-  $scope.options = {
-    scales: {
-      yAxes: [{
-        id: 'y-axis-1',
-        type: 'linear',
-        display: true,
-        position: 'left'
-      }, {
-        id: 'y-axis-2',
-        type: 'linear',
-        display: true,
-        position: 'right'
-      }]
-    }
-  };
-}
-
-angular.module('einstock.dashboard', [
-    'ngMaterial',
-    'highcharts-ng',
-    'chart.js'
-  ])
-  // Dashboard for users to see charts
-  .controller('dashboardController', dashboardCtrl)
-  .controller('stockController', stockCtrl)
-  .controller('lineController', lineCtrl);
+})();
