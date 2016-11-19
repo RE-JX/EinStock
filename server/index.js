f554f76d7718472e19b3511ff579b531a473a240
Aaron Stevens <astev1989@gmail.com>
2016-11-19 16:34:04 -0500

    separate routes into route.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const port = 8080;
 const path = require('path');
-const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const moment = require('moment');
-const port = 8080;
+//---------------------------------------- 
 const database = require('../database');
 const evaluation = require('../evaluator/simulate.js');
 const dumbAlgo2 = require('../algorithms/dumbAlgo1.js').a2;
@@ -17,6 +17,7 @@ const Logistic = require('../mlas/MLs/logistic.js');
 const NaiveBayes = require('../mlas/MLs/nb.js');
 let predictions;
 
+
 //-----------------middleware---------------
 //------------------------------------------
 app.use(bodyParser.json());
@@ -28,95 +29,13 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname + '/../client')));
 
 app.use('/public', express.static(path.join(__dirname + '/../node_modules')));
-
 //-----------------routes-------------------
 //------------------------------------------
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.join(__dirname + '/../client/index.html'));
 });
 
-app.post('/api/data', (req, res) => {
-  function dateFormat(dateOriginal) {
-    var date = new Date(dateOriginal);
-    date = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    return date;
-  }
-
-  dumbAlgo2(dateFormat(req.body.startDate), dateFormat(req.body.endDate), req.body.ticker)
-    .then((result) => {
-      predictions = result;
-    })
-    .then(() => {
-      evaluation('d', dateFormat(req.body.startDate), dateFormat(req.body.endDate), req.body.ticker, predictions)
-        .then((result) => {
-          res.send(result);
-          console.log('returned result: ', result);
-        })
-    })
-});
-
-app.post('/api/data/knn', (req, res) => {
-  function dateFormat(dateOriginal) {
-    var date = new Date(dateOriginal);
-    date = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    return date;
-  };
-
-  var knn = new Neighbors(dateFormat(req.body.startDate), dateFormat(req.body.endDate), req.body.ticker);
-  knn.preProcess()
-    .then(function() {
-      knn.train();
-    })
-    .then(function() {
-      knn.predict();
-    })
-    .then(function() {
-      return evaluation('d', dateFormat(req.body.startDate), dateFormat(req.body.endDate), req.body.ticker, knn.predictions)
-    })
-    .then((result) => {
-      return database.Simulation.create({ //<------ save in database
-        userId: req.userId,
-        algorithm: 'knn',
-        frequency: result.frequency,
-        startDate: result.startDate,
-        endDate: result.endDate,
-        tickerSymbol: result.tickerSymbol,
-        successRate: result.successRate,
-        inclusionError: result.inclusionError,
-        exclusionError: result.exclusionError,
-        avgReturn: result.avgReturn,
-        cummuReturn: result.cummuReturn,
-        returnStd: result.returnStd,
-        sharpeRatio: result.sharpeRatio,
-        benchmarkReturnSelf: result.benchmarkReturnSelf,
-        benchmarkReturnMarket: result.benchmarkReturnMarket,
-        predictedMoves: result.predictedMoves,
-        actualMoves: result.actualMoves,
-        totalAssetValues: result.totalAssetValues,
-        benchmarkAssetValuesSelf: result.benchmarkAssetValuesSelf,
-        benchmarkAssetValuesMarket: result.benchmarkAssetValuesMarket,
-        returns: result.returns,
-        cashPosition: result.cashPosition,
-        stockSharesOwned: result.stockSharesOwned
-      });
-    })
-    .then(result => {
-      res.send(result);
-    });
-});
-
-app.get('/api/data/knn', (req, res) => { // <-- get all simulations created by this user
-  database.Simulation.findAll({
-    where: {
-      userId: req.query.userId
-    }
-  })
-  .then(function(data) {
-    res.send(data);
-  });
-});
-
-
+require('./routes.js')(app);
 //-----------------database-----------------
 //------------------------------------------
 
