5ab4ab2c3d53be738a5997e890251bed4984ad3d
Aaron Stevens <astev1989@gmail.com>
2016-11-23 14:13:06 -0500

    implement load bar inside algorithm.js and algorithm.html

--- client/algorithm/algorithm.js
+++ client/algorithm/algorithm.js
@@ -24,22 +24,27 @@
     $scope.data = {
       startDate: firstDate,
       endDate: new Date(),
-      ticker: '',
+      ticker: 'GOOG',
       algorithm: $scope.selection,
       userId: angular.fromJson(localStorage.getItem('profile')).identities[0].user_id
-    }
+    };
 
     $scope.log = function() {
+      $scope.isLoading = true;
       Algorithm.post($scope.data).success(function(data) {
         console.log(data);
+        Algorithm.get($scope.data.userId).success(function(data) {
+          console.log(data)
+        });
         localStorage.setItem('data', angular.toJson(data));
+        $scope.isLoading = false;
         Algorithm.redirect();
-      })
-    }
+      });
+    };
 
     $scope.tickTest = function() {
      return TickValidation.isValid($scope.data.ticker);
-    }
+    };
 
   }
 
