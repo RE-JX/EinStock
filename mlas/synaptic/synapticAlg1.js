addea629ec984a95fbbd7dda91f5b4656a180331
lhawes <doubletigerstripe@gmail.com>
2016-11-22 19:01:25 -0800

    Style : Cleaning up comments.

--- mlas/synaptic/synapticAlg1.js
+++ mlas/synaptic/synapticAlg1.js
@@ -1,4 +1,4 @@
-var synaptic = require('../../node_modules/synaptic'); // this line is not needed in the browser
+var synaptic = require('../../node_modules/synaptic');
 /* jshint ignore:start */
 var Neuron = synaptic.Neuron;
 var Layer = synaptic.Layer;
@@ -6,14 +6,14 @@ var Network = synaptic.Network;
 /* jshint ignore:end */
 var Trainer = synaptic.Trainer;
 var Architect = synaptic.Architect;
-var Promise = require("bluebird");
+var Promise = require('bluebird');
 
 //Data and normalization
 var normalizer = require('../normalizers').normalizer;
 var worker = require('../../worker').yahoo.historical;
-// var stockTraining = require('../sampleData/aapl6').data;
 
-var NN = (symbol = 'AAPL', from = '2014-01-01', to = '2014-01-10', callback) => {
+var NN = function (symbol, from, to, callback) {
+  'use strict';
 
   var yearBefore = from.split('-');
   yearBefore[0] = String(Number(yearBefore[0]) - 1);
@@ -23,20 +23,17 @@ var NN = (symbol = 'AAPL', from = '2014-01-01', to = '2014-01-10', callback) => 
 
   // console.log(duration, from, to, yearBefore);
 
-
+console.log(symbol, from, to);
 
   worker(symbol, yearBefore, to).then(function(data) {
     var normalizedData = normalizer(data, ['stock', 'symbol', 'date']);
 
-    // console.log(normalizedData);
     //Setup NN and Trainer
-    // var myNetwork = new Architect.Perceptron(6,7,1);
     var myNetwork = new Architect.Perceptron(normalizedData[0].length,normalizedData[0].length * 1, 1);
     var trainer = new Trainer(myNetwork);
 
     //Create a training set
     var trainingSet = normalizedData.reduce((arr, datum, i, a) => {
-      // console.log(datum);
       if (normalizedData.length - 1 !== i) {
         var temp = {input: datum};
         temp.output = [+ (a[i + 1][5] > datum[5])];
@@ -57,41 +54,18 @@ var NN = (symbol = 'AAPL', from = '2014-01-01', to = '2014-01-10', callback) => 
       schedule: {
         every: 1000, // repeat this task every 500 iterations
         do: function(data) {
-          // console.log('error', data.error, 'iterations', data.iterations, 'rate', data.rate);
-          // if (data.error > 0.5) {
-          //   data.rate = 0.5;
-          // } else if (data.error > 0.3) {
-          //   data.rate = 0.3;
-          // } else {
-          //   data.rate = 0.1;
-          // }
 console.log('Percent complete: ', Math.round((data.iterations/100000) * 100), '%');
         }
       }
     });
 
-
-    // normalizedData.forEach((data, i, arr) => {
-    //   if (i !== normalizedData.length - 1) {
-    //     var prediction = Math.round(myNetwork.activate(data)); // 1 means an increase in price
-    //     var actual = (arr[i + 1][5] - arr[i][5]) > 0;  // true means an increase in price
-    //     correct += prediction === +actual ? 1 : 0;
-    //     entries++;
-    //   if (i % 5 === 0) {
-    //     // console.log('prediction:', prediction, 'actual:', actual);
-    //     console.log(correct/normalizedData.length * 100,'%');
-    //     }
-    //   }
-    // });
   var algPredictions = [];
 
   for (var i = normalizedData.length - 1; i >= normalizedData.length - duration; i--) {
 
     algPredictions.push(Math.round(myNetwork.activate(normalizedData[i])));
   }
-// console.log(algPredictions);
-  callback(algPredictions);
-    // console.log('NN got:', correct/normalizedData.length * 100, '%', correct, normalizedData.length);
+  callback(null, algPredictions);
   });
 };
 
