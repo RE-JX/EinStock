28d7bee403ad9577276e564b1d5f43b8506518b0
Natasha Che <natasha@natashache.com>
2016-11-30 00:26:31 -0500

    Add trading activity timeline

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -1,3 +1,4 @@
+
 (function() {
   'use strict'
 
@@ -9,18 +10,10 @@
     // Dashboard for users to see charts
     .controller('SpeedController', SpeedController)
     .controller('DashboardController', DashboardController)
-    .controller('RadarController', RadarController)
-    .controller('LineController', LineController)
-    .controller('PieController', PieController)
-    .controller('BubbleController', BubbleController)
     .controller('TimelineController', TimelineController);
 
   SpeedController.$inject = ['$scope', '$timeout', '$mdDialog'];
   DashboardController.$inject = ['$scope', 'Algorithm'];
-  RadarController.$inject = ['$scope'];
-  LineController.$inject = ['$scope'];
-  PieController.$inject = ['$scope'];
-  TimelineController.$inject = ['$scope'];
 
   //Sidebar navigation controller
   function SpeedController($scope, $timeout, $mdDialog) {
@@ -31,6 +24,23 @@
     };
   };
 
+  function TimelineController($scope) {
+    var history = angular.fromJson(localStorage.getItem('data'));
+    $scope.history = history;
+    var data = history.data;
+    $scope.data = data[data.length - 1];
+    $scope.format = 'YYYY-MM-DD';
+    $scope.startDate = $scope.data.dateLabels[0];
+    $scope.endDate = $scope.data.dateLabels[$scope.data.dateLabels.length - 1];
+    $scope.events = [];
+    $scope.data.buyOrSell.forEach((event, i) => {
+      if(event !== 'hold') {
+        $scope.events.push({'date': $scope.data.dateLabels[i], 'content': event});
+      }
+    });
+    console.log($scope.events);
+  };
+
   //Using to pass local storage to scope of all charts in dashboard
   function DashboardController($scope, Algorithm) {
 
@@ -39,35 +49,9 @@
     var data = history.data;
     $scope.data = data[data.length - 1];
     $scope.data.tickerSymbol = $scope.data.tickerSymbol.toUpperCase();
-    $scope.labels = [];
+    $scope.labels = $scope.data.dateLabels;
     // console.log($scope.data);
     var data = $scope.data;
-
-    //Create labels for dates given and pass to children in scope
-
-    // var current = moment(data.startDate).add(1, 'days');
-    // var end = moment(data.endDate).add(1, 'days');
-    // var labelCount = data.actualMoves.length;
-    // //If end predicted date is weekend, change to following Monday
-    // if (6 <= end.day()) {
-    //   end = end.weekday(8);
-    // }
-
-    // //Way to get day labels
-    // while (labelCount > 1) {
-    //   if (0 < current.day() && current.day() < 6) {
-    //     $scope.labels.push(current.format('YYYY-MM-DD'));
-    //     current.add(1, 'days');
-    //   }
-    //   if (6 <= current.day()) {
-    //     current = current.weekday(8);
-    //     $scope.labels.push(current.format('YYYY-MM-DD'));
-    //     current.add(1, 'days');
-    //   }
-    //   labelCount--;
-    // }
-    $scope.labels = data.dateLabels;
-  };
 
     // History changing function for button module
     $scope.changeHistoryOne = function() {
@@ -84,39 +68,7 @@
       var data = history.data;
       $scope.data = data[data.length - 1 - 4];
       graphRender();
-
-  function TimelineController($scope) {
-    var data = $scope.data;
-    $scope.format = 'YYYY-MM-DD';
-    $scope.startDate = data.dateLabels[0];
-    $scope.endDate = data.dateLabels[data.dateLabels.length - 1];
-    $scope.events = [];
-    data.buyOrSell.forEach((event, i) => {
-      if(event !== 'hold') {
-        $scope.events.push({'date': data.dateLabels[i], 'content': event});
-      }
-    });
-    console.log($scope.events);
-  };
-
-  //Chart top left bar chart to show if it went up or down
-  function RadarController($scope) {
-    var data = $scope.data;
-    $scope.labels = ['Algorithm Cumulative Return', 'Algorithm Average Return', 'Sharpe Ratio', 'Market Benchmark', 'Self Benchmark'];
-    $scope.colors = ['#FDB45C'];
-    $scope.radar = [];
-    $scope.options = {
-      scale: {
-        pointLabels: {
-          fontSize: 16
-        },
-      },
-      tooltips: {
-        titleFontSize: 20,
-        bodyFontSize: 20
-      }
     };
-
 
     //Graph render function to help with re-rendering
     var graphRender = function() {
@@ -133,29 +85,6 @@
         tooltips: {
           titleFontSize: 20,
           bodyFontSize: 20
-
-  //Line chart on top right to show returns
-  function LineController($scope) {
-    var data = $scope.data;
-    $scope.labels = data.dateLabels;
-    $scope.line = [
-      data.benchmarkAssetValuesMarket,
-      data.benchmarkAssetValuesSelf,
-      data.totalAssetValues
-    ];
-
-    $scope.series = [
-      'Benchmark 1: Buy and hold S&P 500',
-      'Benchmark 2: Buy and hold current stock',
-      'Trading from algorithm predictions'
-    ];
-
-    $scope.options = {
-      legend: {
-        display: true,
-        fullWidth: true,
-        labels: {
-          fontSize: 25
         }
       };
       //Add to Radar data
