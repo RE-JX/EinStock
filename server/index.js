ff6de1e63c03f9537cced8dbe4180e1c2adb680f
Natasha Che <natasha@natashache.com>
2016-11-30 21:35:14 -0500

    Fix file paths

--- server/index.js
+++ server/index.js
@@ -20,14 +20,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static(path.join(__dirname + '/../client')));
-
-app.use('/public', express.static(path.join(__dirname + '/../node_modules')));
+app.use(express.static(path.join(__dirname , '/../client')));
+ console.log(path.join(__dirname , '/../client'));
+app.use('/public', express.static(path.join(__dirname , '/../node_modules')));
 
 // //-----------------routes-------------------
 // //------------------------------------------
 app.get('/', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname + '/../client/index.html'));
+  res.status(200).sendFile(path.join(__dirname , '/../client/index.html'));
 });
 
 require('./routes.js')(app);
